5419a9e4b9f98f982699a2a531319bd9d4b3ab93
thanhnenha2 <ta.coaching.v2@gmail.com>
2025-10-22 16:06:27 +0000

    Refactor GoArtPage component to improve header structure; wrap artwork transformation title and star image in a span for better layout consistency.

--- src/app/goart/page.tsx
+++ src/app/goart/page.tsx
@@ -129,12 +129,12 @@ export default function GoArtPage() {
             className="main-image"
           />
         </div>
-        <h1>Transform Your Photo into Artwork
-          <Image
-            src={starImage}
-            alt="Star image"
-            className="star-image"
-          /></h1>
+        <h1><span>Transform Your Photo into Artwork  <Image
+          src={starImage}
+          alt="Star image"
+          className="star-image"
+        /></span>
+        </h1>
         <div className="update-image-container">
           <button className="update-image-button">
             <ImagePlus />
